37284bb5cfd9a5ded7999f45c852fa329c061a20
janakiram775 <janakiram775@gmail.com>
2020-07-24 12:45:33 +0530

    commit

---
+++ Files/cs/EntityEventListner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terrasoft.Core.Entities;
+using Terrasoft.Core.Entities.Events;
+
+namespace DevPackageClio.Files.cs
+{
+    [EntityEventListener(SchemaName ="Contacts")]
+    public class EntityEventListner: BaseEntityEventListener
+    {
+        public override void OnSaved(object sender, EntityAfterEventArgs e)
+        {
+            base.OnSaved(sender, e);
+            var entity = (Entity)sender;
+            var contactName = entity.GetTypedColumnValue<string>("Name");
+            var oldContactName = entity.GetTypedOldColumnValue<string>("Name");
+            entity.SetColumnValue("Name", contactName + "Modified On" + DateTime.Now);
+        }
+    }
+}
