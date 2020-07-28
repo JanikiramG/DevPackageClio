243ae85297d91f3cfcd4abcb66200dcebadf1844
janakiram775 <janakiram775@gmail.com>
2020-07-28 14:11:29 +0530

    commit

--- Files/MyLogic.cs
+++ Files/MyLogic.cs
@@ -14,7 +14,7 @@ namespace DevPackageClio.Files
         public int MyMethod(int a, int b)
         {
             return (a + b) * 10;
-            //test112
+            //test11
         }
     }
 }
